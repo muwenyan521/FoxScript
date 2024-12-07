1e3f0a7d8aa3a252adb02373eee483e384caa486
APIBox <LinkerVB@outlook.com>
2024-12-07 13:26:05 +0800

    😶 新增对FormatException的错误信息

--- Errors/Number.vb
+++ Errors/Number.vb
@@ -1,6 +1,7 @@
 ﻿Public Class NumberError
     Public Shared ErrorDictionary As New Dictionary(Of Type, String) From
     {
-        {GetType(OverflowException), "数值溢出"}
+        {GetType(OverflowException), "数值溢出"},
+        {GetType(FormatException), "不是一个数值"}
     }
 End Class
