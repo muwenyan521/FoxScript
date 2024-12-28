b33b77197bd2bff211a6d249d0f8b36fce4febbe
APIBox <LinkerVB@outlook.com>
2024-12-28 14:13:46 +0800

    😪 REPL启动时显示字符画

--- Module1.vb
+++ Module1.vb
@@ -3,40 +3,66 @@ Imports System.Text
 Imports System.Windows.Forms
 
 Public Class Runner
+    Private AppPath = $"{Application.StartupPath}\"
     Public Mode = "REPL"
     Public FilePath As String = ""
+
+    Public Shared Function Eval(code As String, env As Environment)
+        Dim lexer = New Lexer(code)
+        Dim parser = New Parser(lexer)
+        Dim analyzer = New SemanticAnalyzer()
+        Dim evaluator As New Evaluator
+
+        Dim program As Program = parser.ParseProgram
+        If parser.CheckParserErrors Then Return Nothing
+
+        analyzer.Analysis(program)
+        If analyzer.CheckErrors Then Return Nothing
+
+        Dim result As Fox_Object = evaluator.Eval(program, env)
+        Return result
+    End Function
+
     Public Sub Run()
         Dim env As New Environment
+        env.SetValue(
+            "ModulePath",
+            Eval($"[{$"""{AppPath}\includes\"}""]", env),
+            True
+        )
+
         Dim evaluator As New Evaluator
         Dim lexer As Lexer
         Dim parser As Parser
         Dim analyzer As SemanticAnalyzer
 
         If Mode = "REPL" Then
+            Dim art As String =
+                "  _____                    ____                  _           _   " & vbCrLf &
+                " |  ___|   ___   __  __   / ___|    ___   _ __  (_)  _ __   | |_ " & vbCrLf &
+                " | |_     / _ \  \ \/ /   \___ \   / __| | '__| | | | '_ \  | __|" & vbCrLf &
+                " |  _|   | (_) |  >  <     ___) | | (__  | |    | | | |_) | | |_ " & vbCrLf &
+                " |_|      \___/  /_/\_\   |____/   \___| |_|    |_| | .__/   \__|" & vbCrLf &
+                "                                                    |_|           " & vbCrLf
+            Console.WriteLine(art)
+
             While True
                 Console.Write(">>> ")
                 Dim input = Console.ReadLine() & vbCrLf
                 Dim multiline_result = ReadMultiLineInput(input)
                 If multiline_result <> "" Then input = multiline_result
 
-                lexer = New Lexer(input)
-                parser = New Parser(lexer)
-                analyzer = New SemanticAnalyzer()
-
-                Dim program As Program = parser.ParseProgram
-                If parser.CheckParserErrors Then Continue While
-
-                analyzer.Analysis(program)
-                If analyzer.CheckErrors Then Continue While
-
-                Dim result As Fox_Object = evaluator.Eval(program, env)
+                Dim result = Eval(input, env)
                 If result IsNot Nothing Then
                     Console.WriteLine(result.Inspect)
                 End If
-
             End While
         Else
-            If Not File.Exists(FilePath.Replace("""", "")) Then Return '文件不存在就退出
+            If Not File.Exists(FilePath.Replace("""", "")) Then
+                Console.WriteLine($"文件 {FilePath} 不存在!")
+
+                Return '文件不存在就退出
+            End If
 
             Dim text = ""
             Using sr As New StreamReader(FilePath.Replace("""", ""), Encoding.UTF8)
@@ -45,21 +71,19 @@ Public Class Runner
 
             lexer = New Lexer(text)
             parser = New Parser(lexer)
+            analyzer = New SemanticAnalyzer()
 
             '解析程序
             Dim program As Program = parser.ParseProgram()
-            If parser.checkParserErrors Then
-                Return
-            End If
+            If parser.CheckParserErrors Then Return
 
-            '遍历所有Statement
-            For Each stmt As Statement In program.Statements
-                '求值
-                Dim r As Fox_Object = evaluator.Eval(stmt, env)
-                If r IsNot Nothing AndAlso r.Inspect IsNot Nothing Then
-                    Console.WriteLine(r.Inspect)
-                End If
-            Next
+            analyzer.Analysis(program)
+            If analyzer.CheckErrors Then Return
+
+            Dim result As Fox_Object = evaluator.Eval(program, env)
+            If result IsNot Nothing Then
+                Console.WriteLine(result.Inspect)
+            End If
         End If
     End Sub
 
@@ -122,57 +146,33 @@ Public Class Runner
     End Function
 
 
-    Public Function Run(ByRef env As Environment)
+    Public Function RunModule(ByRef env As Environment)
         Dim evaluator As New Evaluator
         Dim lexer As Lexer
         Dim parser As Parser
 
-        If Mode = "REPL" Then
-            While True
-                Console.Write(">>> ")
-                Dim input = Console.ReadLine() & vbCrLf
-                lexer = New Lexer(input)
-                parser = New Parser(lexer)
+        If Not File.Exists(FilePath.Replace("""", "")) Then Return Nothing '文件不存在就退出
 
-                Dim program As Program = parser.ParseProgram
-                If parser.checkParserErrors Then Continue While
+        Dim text = ""
+        Using sr As New StreamReader(FilePath.Replace("""", ""), Encoding.UTF8)
+            text = sr.ReadToEnd '读取文本
+        End Using
 
-                Dim result As Fox_Object = evaluator.Eval(program, env)
-                If result IsNot Nothing Then
-                    Console.WriteLine(result.Inspect)
-                End If
+        lexer = New Lexer(text)
+        parser = New Parser(lexer)
 
-            End While
-        Else
-            If Not File.Exists(FilePath.Replace("""", "")) Then Return Nothing '文件不存在就退出
-
-            Dim text = ""
-            Using sr As New StreamReader(FilePath.Replace("""", ""), Encoding.UTF8)
-                text = sr.ReadToEnd '读取文本
-            End Using
-
-            lexer = New Lexer(text)
-            parser = New Parser(lexer)
-
-            '解析程序
-            Dim program As Program = parser.ParseProgram()
-            If parser.checkParserErrors Then
-                Return Nothing
-            End If
-
-            '遍历所有Statement
-            For Each stmt As Statement In program.Statements
-                '求值
-                Dim r As Fox_Object = evaluator.eval(stmt, env)
-                If r IsNot Nothing AndAlso r.Inspect IsNot Nothing Then
-                    Console.WriteLine(r.Inspect)
-                End If
-            Next
-
-            Return program.Statements
+        '解析程序
+        Dim program As Program = parser.ParseProgram()
+        If parser.CheckParserErrors Then
+            Return Nothing
         End If
 
-        Return Nothing
+        Dim result As Fox_Object = evaluator.Eval(program, env)
+        If result IsNot Nothing Then
+            Console.WriteLine(result.Inspect)
+        End If
+
+        Return program.Statements
     End Function
 End Class
 
